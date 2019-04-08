e042236f1ad89662e5ccdd4652cc46f0a005b96f
Milosz Sobiczewski <sobiczewsk.milosz@gmail.com>
2019-04-09 00:32:16 +0200

    getter done

--- MovieRestApi/movies/models.py
+++ MovieRestApi/movies/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Movie(models.Model):
     movie_title = models.CharField(max_length=100)
+    omdb_details = models.TextField()
 
     def __str__(self):
         return self.movie_title
--- MovieRestApi/movies/serializers.py
+++ MovieRestApi/movies/serializers.py
@@ -3,20 +3,27 @@ from .models import Movie, Comment
 import requests as r
 import pdb
 
+
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = ('id', 'movie_title')
 
     def create(self, validated_data):
+
+        # custom validation
+
         instance = Movie.objects.create(**validated_data)
 
         movie_title = instance.movie_title
 
         # get external movie data from OMDb API
+        # add configuration file... maybe
         api_key = '120e2295'
         url = 'http://www.omdbapi.com/?t=%s&apikey=%s' % (movie_title, api_key)
         print(url)
+
+        # check for response code
         response = r.get(url)
         if response.status_code == '200':
             print('OK')
@@ -25,8 +32,9 @@ class MovieSerializer(serializers.ModelSerializer):
 
         data = response.json()
         print(data)
-        pdb.set_trace()
+        # pdb.set_trace()
 
+        instance.omdb_details = data
         instance.save()
 
         return instance
