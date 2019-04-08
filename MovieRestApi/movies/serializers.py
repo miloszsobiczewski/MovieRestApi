4341a0835bc89777c88211a9adf3d4639063ca77
Milosz Sobiczewski <sobiczewsk.milosz@gmail.com>
2019-04-08 16:23:09 +0200

    serializers

---
+++ MovieRestApi/movies/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+from .models import Movie, Comment
+
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = ('id', 'movie_title')
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ('id', 'comment_txt')
+
