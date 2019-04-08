d1517dcb074b5feb01b3f8fd16eefb7241ecdda0
Milosz Sobiczewski <sobiczewsk.milosz@gmail.com>
2019-04-08 17:58:47 +0200

    model update

--- MovieRestApi/movies/models.py
+++ MovieRestApi/movies/models.py
@@ -5,8 +5,14 @@ from django.db import models
 class Movie(models.Model):
     movie_title = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.movie_title
+
 
 class Comment(models.Model):
     movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
     comment_txt = models.TextField()
+    date = models.DateField(auto_now=True, blank=False)
 
+    def __str__(self):
+        return self.movie_id
